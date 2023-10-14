608bf480d9fb67e5a1cd110d47a9bdfc0234f936
Ben Walpole <benwalpole95@gmail.com>
2023-10-14 20:00:38 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener("scroll", reveal);
+window.onload = reveal();
+
+function reveal() {
+  const nav = document.querySelector("#nav-menu");
+  var revealTop = wedding.getBoundingClientRect().top;
+
+  function onPageLoad(str) {
+    if (
+      !nav.classList.contains("nav-active") &&
+      !nav.classList.contains("nav-inactive")
+    ) {
+      nav.classList.add(str);
+    }
+  }
+
+  if (revealTop <= 150) {
+    onPageLoad("nav-active");
+    if (nav.classList.contains("nav-inactive")) {
+      nav.classList.add("nav-active");
+      nav.classList.remove("nav-inactive");
+    }
+  } else if (revealTop > 150) {
+    onPageLoad("nav-inactive");
+    if (nav.classList.contains("nav-active")) {
+      nav.classList.remove("nav-active");
+      nav.classList.add("nav-inactive");
+    }
+  }
+}
+
+function scrollToSectionById(id) {
+  const section = document.getElementById(id);
+  section.scrollIntoView();
+}
