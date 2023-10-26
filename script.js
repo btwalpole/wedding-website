7a4023edde46e7fc9f822190ae7d619f71e580ab
Ben Walpole <benwalpole95@gmail.com>
2023-10-26 09:13:15 +0100

    more stuff

--- script.js
+++ script.js
@@ -29,12 +29,50 @@ function reveal() {
   }
 }
 
-function scrollToSectionById(id) {
-  const section = document.getElementById(id);
-  section.scrollIntoView();
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+function myClick(id) {
+  navLinks.forEach((link) => {
+    if (link.id === id) {
+      link.classList.add("active-link");
+    } else {
+      link.classList.remove("active-link");
+    }
+  });
 }
 
-function scrollToWedding() {
-  const section = document.getElementById("wedding");
-  section.scrollIntoView();
-}
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", function () {
+    const idToClick = navLink.getAttribute("href").substring(1);
+    myClick(idToClick);
+  });
+});
+// window.onscroll = () => {
+//   console.log("sections", sections);
+//   console.log("navLinks", navLinks);
+//   sections.forEach((sec) => {
+//     console.log("section", sec);
+
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 150;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+//     if (top >= offset && top < offset + height) {
+//       navLinks.forEach((links) => {
+//         console.log("in navLinks forEach");
+//         links.classList.remove("active-link");
+//         document
+//           .querySelector("header nav a[href*=" + id + "]")
+//           .classList.add("active-link");
+//       });
+//     }
+//   });
+// };
+
+// function scrollToSectionById(id) {
+//   const section = document.getElementById(id);
+//   section.scrollIntoView();
+// }
+
+// onclick="scrollToSectionById(`wedding`)"
