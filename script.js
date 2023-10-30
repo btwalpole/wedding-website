0df36da0ff2ec49f4024f2ffb8348e11ba4afdf5
Ben Walpole <benwalpole95@gmail.com>
2023-10-30 09:44:38 +0000

    fixing nav bar bug

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.addEventListener("scroll", reveal);
 window.onload = reveal();
 
 function reveal() {
-  console.log("running reveal function");
   const nav = document.querySelector("#nav-menu");
   var revealTop = wedding.getBoundingClientRect().top;
 
@@ -33,9 +32,9 @@ function reveal() {
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
-function myClick(id) {
+function onClickLink(id) {
   navLinks.forEach((link) => {
-    if (link.id === id) {
+    if (link.getAttribute("href").substring(1) === id) {
       link.classList.add("active-link");
     } else {
       link.classList.remove("active-link");
@@ -46,34 +45,11 @@ function myClick(id) {
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", function () {
     const idToClick = navLink.getAttribute("href").substring(1);
-    myClick(idToClick);
+    onClickLink(idToClick);
   });
 });
-// window.onscroll = () => {
-//   console.log("sections", sections);
-//   console.log("navLinks", navLinks);
-//   sections.forEach((sec) => {
-//     console.log("section", sec);
 
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop - 150;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute("id");
-//     if (top >= offset && top < offset + height) {
-//       navLinks.forEach((links) => {
-//         console.log("in navLinks forEach");
-//         links.classList.remove("active-link");
-//         document
-//           .querySelector("header nav a[href*=" + id + "]")
-//           .classList.add("active-link");
-//       });
-//     }
-//   });
-// };
-
-// function scrollToSectionById(id) {
-//   const section = document.getElementById(id);
-//   section.scrollIntoView();
-// }
-
-// onclick="scrollToSectionById(`wedding`)"
+const chevron = document.getElementById("chevron");
+chevron.addEventListener("click", function () {
+  onClickLink("wedding");
+});
