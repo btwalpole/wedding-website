230df9ae48f766955ea09bc1f83307e01e22bafa
Ben Walpole <benwalpole95@gmail.com>
2023-11-19 20:17:24 +0000

    fixed nav bar

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-window.addEventListener("scroll", reveal);
-window.onload = reveal();
-
 const leftTop = document.getElementById("left-top");
 const rightTop = document.getElementById("right-top");
 
 window.onscroll = function () {
   var scrollLimit = 100;
-
-  console.log("scrolling");
-  console.log(leftTop);
-  console.log("rightTop", rightTop);
-
   if (window.scrollY >= scrollLimit) {
-    console.log("past scrollLimit");
-
     if (
       !leftTop.classList.contains("visible") &&
       !rightTop.classList.contains("visible")
@@ -23,8 +13,6 @@ window.onscroll = function () {
     }
   }
   if (window.scrollY < scrollLimit) {
-    console.log("above scrollLimit");
-
     if (
       leftTop.classList.contains("visible") &&
       leftTop.classList.contains("visible")
@@ -35,36 +23,8 @@ window.onscroll = function () {
   }
 };
 
-function reveal() {
-  const nav = document.querySelector("#nav-menu");
-  var revealTop = wedding.getBoundingClientRect().top;
-
-  function onPageLoad(str) {
-    if (
-      !nav.classList.contains("nav-active") &&
-      !nav.classList.contains("nav-inactive")
-    ) {
-      nav.classList.add(str);
-    }
-  }
-
-  if (revealTop <= 150) {
-    onPageLoad("nav-active");
-    if (nav.classList.contains("nav-inactive")) {
-      nav.classList.add("nav-active");
-      nav.classList.remove("nav-inactive");
-    }
-  } else if (revealTop > 150) {
-    onPageLoad("nav-inactive");
-    if (nav.classList.contains("nav-active")) {
-      nav.classList.remove("nav-active");
-      nav.classList.add("nav-inactive");
-    }
-  }
-}
-
 let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+let navLinks = document.querySelectorAll("nav a");
 
 function onClickLink(id) {
   navLinks.forEach((link) => {
@@ -85,5 +45,36 @@ navLinks.forEach((navLink) => {
 
 const chevron = document.getElementById("chevron");
 chevron.addEventListener("click", function () {
-  onClickLink("wedding");
+  onClickLink("our-story");
 });
+
+// window.addEventListener("scroll", reveal);
+// window.onload = reveal();
+
+// function reveal() {
+//   const nav = document.querySelector("#nav-menu");
+//   var revealTop = wedding.getBoundingClientRect().top;
+
+//   function onPageLoad(str) {
+//     if (
+//       !nav.classList.contains("nav-active") &&
+//       !nav.classList.contains("nav-inactive")
+//     ) {
+//       nav.classList.add(str);
+//     }
+//   }
+
+//   if (revealTop <= 150) {
+//     onPageLoad("nav-active");
+//     if (nav.classList.contains("nav-inactive")) {
+//       nav.classList.add("nav-active");
+//       nav.classList.remove("nav-inactive");
+//     }
+//   } else if (revealTop > 150) {
+//     onPageLoad("nav-inactive");
+//     if (nav.classList.contains("nav-active")) {
+//       nav.classList.remove("nav-active");
+//       nav.classList.add("nav-inactive");
+//     }
+//   }
+// }
